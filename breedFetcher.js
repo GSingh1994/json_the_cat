3a9495b50833a1cb6c7048ca0d697157913dd237
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-14 11:42:58 -0500

    Add mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,12 @@ const request = require('request');
 
 const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
-    callback(error, body);
+    if (error) {
+      callback(error);
+    } else {
+      const data = JSON.parse(body);
+      data.length > 0 ? callback(error, data[0].description) : callback(false);
+    }
   });
 };
 
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 const breedName = process.argv[2];
 
-fetchBreedDescription(breedName, (error, body) => {
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log(`"${error.hostname}" not found (Request failed successfully!!)`);
   } else {
-    const data = JSON.parse(body);
-    data.length > 0 ? console.log(data[0].description) : console.log(`${breedName} Breed not found`);
+    console.log(error);
+    desc ? console.log(desc) : console.log(`${breedName} Breed not found`);
   }
 });
